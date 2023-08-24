3e17361ac9a47f354d8850010a85f431f72a823e
priskilladrn <priskilla.adriani@gmail.com>
2023-08-24 15:48:22 +0700

    crown view

--- Tingle Watch App/CrownView.swift
+++ Tingle Watch App/CrownView.swift
@@ -5,4 +5,36 @@
 //  Created by Azella Mutyara on 24/08/23.
 //
 
-import Foundation
+import SwiftUI
+
+struct CrownView: View {
+    @State var scrollAmount = 0.0
+    
+    var body: some View {
+        GeometryReader { geo in
+            VStack {
+                Spacer()
+                HStack {
+                    Spacer()
+                    Image("1")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: geo.size.width * 0.5)
+                        .cornerRadius(16)
+                        .focusable(true)
+                        .rotationEffect(Angle(degrees: scrollAmount))
+                        .digitalCrownRotation($scrollAmount, from: 1, through: 360, by: 15.0, sensitivity: .high, isContinuous: true, isHapticFeedbackEnabled: true)
+                    Spacer()
+                }
+                Spacer()
+            }
+        }
+        .ignoresSafeArea()
+    }
+}
+
+struct CrownView_Previews: PreviewProvider {
+    static var previews: some View {
+        CrownView()
+    }
+}
