016623a954f7073bd66785956768dfa831139224
Azella Gania Mutyara <azellamutyara@gmail.com>
2023-08-24 10:59:43 +0700

    Initial Commit

---
+++ Tingle Watch App/TingleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TingleApp.swift
+//  Tingle Watch App
+//
+//  Created by Azella Mutyara on 24/08/23.
+//
+
+import SwiftUI
+
+@main
+struct Tingle_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
