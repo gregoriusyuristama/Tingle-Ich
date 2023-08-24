afe0549ad134cd045d55426dea6f33b9c888f394
Azella Gania Mutyara <azellamutyara@gmail.com>
2023-08-24 11:09:27 +0700

    Add View file for gyro, crown, and tap

---
+++ Tingle Watch App/GyroView.swift
@@ -0,0 +1,20 @@
+//
+//  GyroView.swift
+//  Tingle Watch App
+//
+//  Created by Azella Mutyara on 24/08/23.
+//
+
+import SwiftUI
+
+struct GyroView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GyroView_Previews: PreviewProvider {
+    static var previews: some View {
+        GyroView()
+    }
+}
